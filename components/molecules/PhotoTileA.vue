4d9eb7cb06d9f9afb2d0ad8f516230322056d52d
Yoshimura Naoya <yoshimura708x@gmail.com>
2022-02-15 01:57:36 +0900

    feat: make .vue file for PhotoTileA component

---
+++ components/molecules/PhotoTileA.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-card> TBA </v-card>
+</template>
+
+<script scoped>
+import config from '@/assets/config'
+
+export default {
+  name: 'PhotoTileA',
+  props: {
+    imgSrc: {
+      type: String,
+      default: 'TBA',
+    },
+    txtBody: {
+      type: String,
+      default: 'body text',
+    },
+    // links: {
+    //   type: Array,
+    //   default: ['TBA'],
+    // },
+    bgcolor: {
+      type: String,
+      default: '#ffffff',
+    },
+  },
+  data: () => ({
+    brandName: config.brand.nameShort,
+  }),
+}
+</script>
+
+<style lang="scss">
+@import '@/assets/styles/app';
+</style>
