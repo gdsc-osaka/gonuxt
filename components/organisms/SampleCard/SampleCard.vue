a5f91c41640df5f1462dc66e0bd27009055a4e24
Ryo Shirai <rbasket75jam@gmail.com>
2022-02-20 11:57:57 +0900

    tutorial: resotored components

---
+++ components/organisms/SampleCard/SampleCard.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-row justify="center" class="text-center" no-gutters>
+    <v-col cols="12" sm="6">
+      <h1 class="font-weight-bold">{{ title }}</h1>
+      <p class="mb-7">{{ txtBody }}</p>
+
+      <div class="mb-2">
+        <v-btn
+          style="text-transform: none"
+          large
+          rounded
+          class="white--text"
+          :color="btnColor"
+          :to="link" 
+        >
+          {{ btnTxt }}
+        </v-btn>
+      </div>
+      <small>{{ subTxt }}</small>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: 'Sample Card',
+    },
+    txtBody: {
+      type: String,
+      default: 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.',
+    },
+    btnTxt: {
+      type: String,
+      default: 'Sample Button Text'
+    },
+    subTxt: {
+      type: String,
+      default: 'Sample Sub Text'
+    },
+    btnColor: {
+      type: String,
+      default: 'black'
+    },
+    link: {
+      type: String,
+      default: '/'
+    }
+  },
+}
+</script>
--- components/templates/SampleLayoutST/SampleLayoutST.vue
+++ components/templates/SampleLayoutST/SampleLayoutST.vue
@@ -3,22 +3,22 @@
     <v-row justify="center" class="my-8" no-gutters>
       <v-col cols="12" sm="11">
         <div class="mb-8">
-          <TitleCenter class="mb-8"/>
+          <SampleCard />
         </div>
-        <v-img :src="imgSrc" class="rounded-lg"/>
+        <v-img :src="imgSrc" class="rounded-lg" />
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
-import TitleCenter from '@/components/organisms/TitleCenter/TitleCenter.vue'
+import SampleCard from '@/components/organisms/SampleCard/SampleCard.vue'
 
 export default {
-  components: { TitleCenter },
+  components: { SampleCard },
   data(){
         return {
-            imgSrc: require("@/assets/image/gonuxt/sample01.jpg")
+            imgSrc: require("@/assets/img/gonuxt/sample01.jpg")
         }
     }
 }
