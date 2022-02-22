f0a440b0b58015a7ce2daf10441d68d6c35557df
Ryo Shirai <rbasket75jam@gmail.com>
2022-02-22 14:07:35 +0900

    feat-02: renamed components #2

---
+++ components/templates/TitleImgLayoutST/script.js
@@ -0,0 +1,10 @@
+import TitleCenter from '@/components/organisms/TitleCenter/TitleCenter.vue'
+
+export default {
+  components: { TitleCenter },
+  data() {
+    return {
+      imgSrc: require('@/assets/img/gonuxt/sample01.jpg'),
+    }
+  },
+}
--- sections/template/TemplateShowcaseSec/script.js
+++ sections/template/TemplateShowcaseSec/script.js
@@ -1,8 +1,8 @@
 import TextCenterST from '@/components/templates/TextCenterST/TextCenterST.vue'
 import TileLayoutST from '@/components/templates/TileLayoutST/TileLayoutST.vue'
-import SampleLayoutST from '@/components/templates/SampleLayoutST/SampleLayoutST.vue'
+import TitleImgLayoutST from '@/components/templates/TitleImgLayoutST/TitleImgLayoutST.vue'
 
 export default {
   name: 'TemplateShowcaseSec',
-  components: { TextCenterST, TileLayoutST, SampleLayoutST },
+  components: { TextCenterST, TileLayoutST, TitleImgLayoutST },
 }
