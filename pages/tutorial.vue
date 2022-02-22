ae087992ff77c071c2a4b2db3d2057ea22d1b113
Yoshimura Naoya <yoshimura708x@gmail.com>
2022-02-22 13:58:58 +0900

    feat: add example files for tutorial

---
+++ pages/tutorial.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container fluid>
+    <TutorialShowcaseSec />
+  </v-container>
+</template>
+
+<script>
+import TutorialShowcaseSec from '@/sections/template/TutorialShowcaseSec/TutorialShowcaseSec/'
+export default {
+  name: 'TemplatePage',
+  components: { TutorialShowcaseSec },
+}
+</script>
---
+++ sections/template/TutorialShowcaseSec/script.js
@@ -0,0 +1,6 @@
+import TextCenterST from '@/components/templates/TextCenterST/TextCenterST.vue'
+
+export default {
+  name: 'TutorialShowcaseSec',
+  components: { TextCenterST },
+}
