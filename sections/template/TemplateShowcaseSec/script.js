34c6d1f9af481b7057d9aecca6076c692325e4d8
Yoshimura Naoya <yoshimura708x@gmail.com>
2022-02-23 14:22:41 +0900

    feat: register CardGalleryLayout

--- sections/template/TemplateShowcaseSec/script.js
+++ sections/template/TemplateShowcaseSec/script.js
@@ -2,8 +2,9 @@ import TextCenterST from '@/components/templates/TextCenterST/TextCenterST.vue'
 import TileLayoutST from '@/components/templates/TileLayoutST/TileLayoutST.vue'
 import TitleImgLayoutST from '@/components/templates/TitleImgLayoutST/TitleImgLayoutST.vue'
 import IntroAST from '@/components/templates/IntroAST/IntroAST.vue'
+import CardGalleryLayoutST from '@/components/templates/CardGalleryLayoutST/CardGalleryLayoutST.vue'
 
 export default {
   name: 'TemplateShowcaseSec',
-  components: { TextCenterST, TileLayoutST, TitleImgLayoutST, IntroAST },
+  components: { TextCenterST, TileLayoutST, TitleImgLayoutST, IntroAST, CardGalleryLayoutST },
 }
