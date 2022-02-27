0351e96f18b541b49656c2831a329edfa0d46f29
Yoshimura Naoya <yoshimura708x@gmail.com>
2022-02-28 02:02:18 +0900

    refactor: move components into category pages

---
+++ pages/templates/lists.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-main>
+    <section class="gnt-sec-2">
+      <v-row>
+        <v-col cols="12" class="text-center font-weight-bold">
+          <h1>Template Showcase (List)</h1>
+        </v-col>
+      </v-row>
+    </section>
+  </v-main>
+</template>
+
+<script scoped>
+export default {
+  name: 'TextPage',
+  head: {
+    meta: [
+      { hid: 'og:title', property: 'og:title', content: 'List' },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content: 'GONuxTテンプレート一覧 (List)',
+      },
+    ],
+  },
+}
+</script>
