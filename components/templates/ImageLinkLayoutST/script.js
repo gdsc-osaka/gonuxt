7d399df3de507f505e3be4f8fb9bfe7e84ba57f6
h-atsu <jhuim08@gmail.com>
2022-03-02 23:02:41 +0900

    made frame

---
+++ components/templates/ImageLinkLayoutST/script.js
@@ -0,0 +1,33 @@
+export default {
+  name: 'ImageLinkLayoutST',
+  data: () => ({
+    tiles2Col: [],
+    tiles4Col: [],
+  }),
+  created: function () {
+    this.tiles2Col = this.getTileData(4)
+  },
+  methods: {
+    getTileData(ncol) {
+      const tiles = []
+      const positions = ['top', 'center', 'bottom']
+      const titleSizeList = ['title-1', 'title-2', 'title-3', 'title-4']
+
+      for (let i = 0; i < ncol; i++) {
+        const titleSize = titleSizeList[i % titleSizeList.length]
+        const imgIndex = (i % 3) + 1
+        const pos = positions[i % positions.length]
+        const link = i % 2 === 0 ? null : { href: '/', txt: 'link text' }
+
+        tiles.push({
+          title: `ImageLinkLayout ${titleSize}`,
+          titleSize: titleSize,
+          imgSrc: require(`@/assets/gonuxt/img/sample0${imgIndex}.jpg`),
+          txtPosition: pos,
+          link: link,
+        })
+      }
+      return tiles
+    },
+  },
+}
--- sections/template/TemplateShowcaseSec/script.js
+++ sections/template/TemplateShowcaseSec/script.js
@@ -2,6 +2,7 @@ import TextCenterST from '@/components/templates/TextCenterST/TextCenterST.vue'
 import TileLayoutST from '@/components/templates/TileLayoutST/TileLayoutST.vue'
 import TitleImgLayoutST from '@/components/templates/TitleImgLayoutST/TitleImgLayoutST.vue'
 import TextCenterBST from '@/components/templates/TextCenterBST/TextCenterBST.vue'
+import ImageLinkLayoutST from '@/components/templates/ImageLinkLayoutST/ImageLinkLayoutST.vue'
 import IntroAST from '@/components/templates/IntroAST/IntroAST.vue'
 import CardGalleryLayoutST from '@/components/templates/CardGalleryLayoutST/CardGalleryLayoutST.vue'
 import ScrollTopBtn from '@/components/organisms/ScrollTopBtn.vue'
@@ -12,6 +13,7 @@ export default {
     TextCenterST,
     TileLayoutST,
     TitleImgLayoutST,
+    ImageLinkLayoutST,
     IntroAST,
     TextCenterBST,
     CardGalleryLayoutST,
