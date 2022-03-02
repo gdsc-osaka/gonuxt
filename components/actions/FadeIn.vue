e8cba13e1b8ab7f98dea6381fcd1acfeff7672be
Ryo Shirai <rbasket75jam@gmail.com>
2022-03-02 12:20:23 +0900

    add fadeIn component #29

---
+++ components/actions/FadeIn.vue
@@ -0,0 +1,28 @@
+<template>
+  <div :id="fadeIn" v-scroll="handleScroll">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FadeIn',
+  props: {
+    fadeIn: {
+      type: String,
+      default: 'fadeIn',
+    },
+  },
+  methods: {
+    handleScroll: function (evt, el, pos) {
+      el = document.getElementById(this.fadeIn)
+      pos = el.getBoundingClientRect().top
+      if (pos < 280) {
+        el.setAttribute('style', 'visibly 1; transition-duration: 1s; transform: translateY(0px);')
+      } else {
+        el.setAttribute('style', 'opacity: 0; transition-duration: 1s; transform: translateY(50px);')
+      }
+    },
+  },
+}
+</script>
