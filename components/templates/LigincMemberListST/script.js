2c4906ecd116038a03fceac0c7223afa48bf5a23
Rikuto Tsubouchi <tsubouchi.rikuto@ist.osaka-u.ac.jp>
2022-03-03 23:33:49 +0900

    write some files

---
+++ components/templates/LigincMemberListST/script.js
@@ -0,0 +1,10 @@
+import LigincCard from '@/components/organisms/LigincCard.vue'
+
+export default {
+    components: { LigincCard },
+    data() {
+      return {
+        imgSrc: require('@/assets/gonuxt/img/sample01.jpg'),
+      }
+    },
+  }
