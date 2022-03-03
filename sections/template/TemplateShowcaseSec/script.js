ba9d0488e5736f91ac46e695f72139a0c216dcaa
h-atsu <jhuim08@gmail.com>
2022-03-04 00:51:57 +0900

    retrieve dev section/ for PR

--- sections/template/TemplateShowcaseSec/script.js
+++ sections/template/TemplateShowcaseSec/script.js
@@ -2,7 +2,6 @@ import TextCenterST from '@/components/templates/TextCenterST/TextCenterST.vue'
 import TileLayoutST from '@/components/templates/TileLayoutST/TileLayoutST.vue'
 import TitleImgLayoutST from '@/components/templates/TitleImgLayoutST/TitleImgLayoutST.vue'
 import TextCenterBST from '@/components/templates/TextCenterBST/TextCenterBST.vue'
-import ImageLinkLayoutST from '@/components/templates/ImageLinkLayoutST/ImageLinkLayoutST.vue'
 import IntroAST from '@/components/templates/IntroAST/IntroAST.vue'
 import CardGalleryLayoutST from '@/components/templates/CardGalleryLayoutST/CardGalleryLayoutST.vue'
 import ScrollTopBtn from '@/components/organisms/ScrollTopBtn.vue'
@@ -13,7 +12,6 @@ export default {
     TextCenterST,
     TileLayoutST,
     TitleImgLayoutST,
-    ImageLinkLayoutST,
     IntroAST,
     TextCenterBST,
     CardGalleryLayoutST,
