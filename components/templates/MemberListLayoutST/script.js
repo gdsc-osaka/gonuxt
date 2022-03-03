537b20b58c71a747f4dd540fe754095c1aa743a6
litchi7777 <litchi7777@gmail.com>
2022-03-03 12:37:20 +0000

    add MemberCard.vue & MemberListLayoutST

---
+++ components/templates/MemberListLayoutST/script.js
@@ -0,0 +1,10 @@
+import MemberCard from '@/components/organisms/MemberCard.vue'
+
+export default {
+  components: { MemberCard },
+  data() {
+    return {
+      imgSrc: require('@/assets/img/gonuxt/sample01.jpg'),
+    }
+  },
+}
