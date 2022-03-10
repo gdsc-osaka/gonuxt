8e8bc51aa8c92082db51e6926022576b13c7e469
Ryo Shirai <rbasket75jam@gmail.com>
2022-03-10 13:54:37 +0900

    add ScrollDownBtn

---
+++ components/organisms/ScrollDownBtn.vue
@@ -0,0 +1,63 @@
+<template>
+  <p class="scroll js-scroll">
+    <a href="#" v-on:click.stop.prevent="openWindow">{{btnTxt}}</a>
+  </p>
+</template>
+
+<script>
+export default {
+  props: {
+    btnTxt: {
+      type: String,
+      default: 'btnTxt',
+    },
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.scroll {
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+  text-align: center;
+  letter-spacing: 0.15em;
+  text-shadow: 1px 0 1px black;
+  a {
+    position: relative;
+    display: inline-block;
+    font-size: 24px;
+    font-weight: bold;
+    color: white;
+    text-decoration: none;
+    padding: 0 0 50px;
+    opacity: 0.8;
+    &:before {
+      content: '';
+      position: absolute;
+      width: 15px;
+      height: 15px;
+      left: 0;
+      right: 0;
+      bottom: 25px;
+      margin: auto;
+      border-bottom: 4px solid white;
+      border-right: 4px solid white;
+      transform: rotate(45deg);
+      animation: move 2s infinite;
+    }
+  }
+}
+
+@keyframes move {
+  0% {
+    transform: rotate(45deg) translate(0, 0);
+  }
+  20% {
+    transform: rotate(45deg) translate(8px, 8px);
+  }
+  40% {
+    transform: rotate(45deg) translate(0, 0);
+  }
+}
+</style>
---
+++ components/templates/AutoSwitchTitle/script.js
@@ -0,0 +1,46 @@
+import ScrollDownBtn from '@/components/organisms/ScrollDownBtn.vue'
+
+export default {
+  components: { ScrollDownBtn },
+  
+  data() {
+    return {
+      currentSlide: 0,
+      slides: [
+        { id: 0, img: require('@/assets/gonuxt/img/sample01.jpg') },
+        { id: 1, img: require('@/assets/gonuxt/img/sample02.jpg') },
+        { id: 2, img: require('@/assets/gonuxt/img/sample03.jpg') },
+      ],
+      fade: 'next',
+      show: true,
+      timer: 0,
+    }
+  },
+  computed: {
+    Slides() {
+      return this.slides.filter((slide) => {
+        return slide.id === this.currentSlide
+      })
+    },
+  },
+  created() {
+    this.timer = setInterval(() => {
+      this.autoPlay()
+    }, 3000)
+  },
+  methods: {
+    autoPlay() {
+      this.currentSlide++
+      if (this.currentSlide === this.slides.length) {
+        this.currentSlide = 0
+      }
+    },
+    openWindow: function (height) {
+      height = document.getElementById('slide-outers').clientHeight
+      window.scrollTo({
+        top: height,
+        behavior: 'smooth',
+      })
+    },
+  },
+}
