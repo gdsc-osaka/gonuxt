091467dbe0358cc912b58e2d9d392047bd6ef645
litchi7777 <litchi7777@gmail.com>
2022-03-10 12:40:38 +0000

    fix img pass

--- components/templates/MemberListLayoutST/script.js
+++ components/templates/MemberListLayoutST/script.js
@@ -8,32 +8,32 @@ export default {
       {name:"田中　太郎",
       txtBody:"（自己紹介本文）- 吾輩は猫である。名前はまだ無い。どこで生れたかとんと見当がつかぬ。何でも薄暗いじめじめした所でニャーニャー泣いていた事だけは記憶している。",
       job:"（役職等）",
-      imgSrc:require('@/assets/img/gonuxt/sample01.jpg')
+      imgSrc:require('@/assets/gonuxt/img/gonuxt/sample01.jpg')
       },
       {name:"田中　太郎",
       txtBody:"（自己紹介本文）- 吾輩は猫である。名前はまだ無い。どこで生れたかとんと見当がつかぬ。何でも薄暗いじめじめした所でニャーニャー泣いていた事だけは記憶している。",
       job:"（役職等）",
-      imgSrc:require('@/assets/img/gonuxt/sample02.jpg')
+      imgSrc:require('@/assets/gonuxt/img/gonuxt/sample02.jpg')
       },
       {name:"田中　太郎",
       txtBody:"（自己紹介本文）- 吾輩は猫である。名前はまだ無い。どこで生れたかとんと見当がつかぬ。何でも薄暗いじめじめした所でニャーニャー泣いていた事だけは記憶している。",
       job:"（役職等）",
-      imgSrc:require('@/assets/img/gonuxt/sample03.jpg')
+      imgSrc:require('@/assets/gonuxt/img/gonuxt/sample03.jpg')
       },
       {name:"田中　太郎",
       txtBody:"（自己紹介本文）- 吾輩は猫である。名前はまだ無い。どこで生れたかとんと見当がつかぬ。何でも薄暗いじめじめした所でニャーニャー泣いていた事だけは記憶している。",
       job:"（役職等）",
-      imgSrc:require('@/assets/img/gonuxt/sample01.jpg')
+      imgSrc:require('@/assets/gonuxt/img/gonuxt/sample01.jpg')
       },
       {name:"田中　太郎",
       txtBody:"（自己紹介本文）- 吾輩は猫である。名前はまだ無い。どこで生れたかとんと見当がつかぬ。何でも薄暗いじめじめした所でニャーニャー泣いていた事だけは記憶している。",
       job:"（役職等）",
-      imgSrc:require('@/assets/img/gonuxt/sample02.jpg')
+      imgSrc:require('@/assets/gonuxt/img/gonuxt/sample02.jpg')
       },
       {name:"田中　太郎",
       txtBody:"（自己紹介本文）- 吾輩は猫である。名前はまだ無い。どこで生れたかとんと見当がつかぬ。何でも薄暗いじめじめした所でニャーニャー泣いていた事だけは記憶している。",
       job:"（役職等）",
-      imgSrc:require('@/assets/img/gonuxt/sample03.jpg')
+      imgSrc:require('@/assets/gonuxt/img/gonuxt/sample03.jpg')
       },
     ]
   }),
