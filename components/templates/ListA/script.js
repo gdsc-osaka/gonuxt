223a0a58f3f70d012b36ab2e82e644308da4201d
Ryo Shirai <rbasket75jam@gmail.com>
2022-03-12 13:34:36 +0900

    add list table

---
+++ components/templates/ListA/script.js
@@ -0,0 +1,22 @@
+import schedules from '@/components/templates/ListA/ListA.json'
+
+export default {
+  data() {
+    return {
+      headers: [
+        { text: '予定', value: 'name', sortable: false },
+        { text: 'イベント・大会・交流戦', value: 'event', sortable: false },
+        { text: '場所', value: 'place', sortable: false },
+        { text: '概要', value: 'overview', sortable: false },
+      ],
+      schedules: schedules,
+    }
+  },
+  methods: {
+    getColor(name) {
+      if (name === '4月') return 'red'
+      else if (name === '5月') return 'orange'
+      else return 'green'
+    },
+  },
+}
