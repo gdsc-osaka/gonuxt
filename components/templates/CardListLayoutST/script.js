db9b841c67dbce790c9068872a6279c38a4cc88e
litchi7777 <litchi7777@gmail.com>
2022-03-31 12:48:00 +0000

    finish

--- components/templates/CardListLayoutST/script.js
+++ components/templates/CardListLayoutST/script.js
@@ -2,13 +2,13 @@ export default {
   name: 'CardListLayoutST',
   data: () => ({
     cards:[
-      {imgSrc:require('@/assets/gonuxt/img/sample01.jpg'),
-      link:"/dev"},
-      {imgSrc:require('@/assets/gonuxt/img/sample01.jpg'),
+      {imgSrc:require('@/assets/gonuxt/img/16-9/sample01.jpg'),
       link:"/"},
-      {imgSrc:require('@/assets/gonuxt/img/sample01.jpg'),
+      {imgSrc:require('@/assets/gonuxt/img/16-9/sample01.jpg'),
       link:"/"},
-      {imgSrc:require('@/assets/gonuxt/img/sample01.jpg'),
+      {imgSrc:require('@/assets/gonuxt/img/16-9/sample01.jpg'),
+      link:"/"},
+      {imgSrc:require('@/assets/gonuxt/img/16-9/sample01.jpg'),
       link:"/"},
     ]
   }),
